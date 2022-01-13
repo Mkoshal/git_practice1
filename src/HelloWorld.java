a900e55b9652ad08f4f78d394b913215448d3fcd
makhabatkoshalieva <baiel192017@gmail.com>
2022-01-13 15:26:06 -0500

    work 2 is done

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,5 +2,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
         // doing some work
+        //doing some awesome work
+        //keep doing like that
     }
 }
